f23665cfa2fc2aa73b5ba4e57a69abbd0edf5946
Andy Sekulovski <andy.sek94@gmail.com>
2018-11-27 17:53:34 +1100

    imported, cleaned code and added gitignore

--- src/BigO/Main.java
+++ src/BigO/Main.java
@@ -6,28 +6,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
         //reverse a string
         reverseString("The quick brown fox JumPs oVer the yellow dog");
-
         reverseString("abcdefghijklmnopqrstuvwxyz");
-
     }
 
     static void reverseString(String input) {
 
         String reversedString = "";
-
         System.out.println("Length of string is: " + input.length());
 
         for(int i=0; i < input.length(); i++) {
-
             char found = input.charAt(input.length() - (i+1));
 
            // System.out.println("letter: " + found);
-
             reversedString += found;
-
         }
         System.out.println(input + " >>> " + reversedString.toLowerCase());
     }
